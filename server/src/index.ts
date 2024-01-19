2077f87724b5e2908b84aed85630e37678146126
aryan017 <68499358+aryan017@users.noreply.github.com>
2024-01-19 22:16:39 +0530

    My authentications is Completed

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { userRouter } from './routes/user'
+
+dotenv.config()
+
+const PORT: number = Number(process.env.PORT);
+const databaseURL: string = process.env.DATABASE_URL 
+const app=express();
+
+app.use(express.json()) // convert all the data coming towards server into JSON format
+app.use(cors()) // Cross Origin Resource Sharing
+app.use("/user",userRouter)  // makes the call to the http://localhost:3001/user/...
+
+mongoose.connect(databaseURL);
+
+app.listen(PORT ,() => {
+    console.log(`server started!!! ${PORT}`)
+})
